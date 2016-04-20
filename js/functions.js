920d7398f7a5e4e37d44615ab6ec195e10972c55
xelz <xelzmm@gmail.com>
2016-04-20 23:41:20 +0800

    add year

--- js/functions.js
+++ js/functions.js
@@ -41,8 +41,15 @@ $(window).resize(function() {
 })(jQuery);
 
 function timeElapse(date){
-	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var current = new Date();
+	var date_in_this_year = new Date(date);
+	date_in_this_year.setFullYear(current.getFullYear());
+	var years = current.getFullYear() - date.getFullYear(), seconds;
+	if(Date.parse(current) < Date.parse(date_in_this_year)) {
+		date_in_this_year.setFullYear(current.getFullYear() - 1);
+		years -= 1;
+	}
+	var seconds = (Date.parse(current) - Date.parse(date_in_this_year)) / 1000;
 	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
@@ -58,6 +65,6 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "<span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
+	var result = "<span class=\"digit\">" + years + "</span> 年 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
 	$("#clock").html(result);
 }
